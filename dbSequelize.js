6a29929dff022e5a180ad033bffd62d7ef92a154
Harmash Maksym <garmashmaxim05@gmail.com>
2025-05-24 21:54:18 +0300

    add dbSequelize.js to gitignore, change app.js to better output in terminal

--- dbSequelize.js
+++ dbSequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('ExchangeRate', '', '', { // 1: 'name', 2: 'user', 3: 'password'
+const sequelize = new Sequelize('ExchangeRate', 'sa', '12345', { // 1: 'name', 2: 'user', 3: 'password'
     dialect: 'mssql',
     host: 'localhost'
 });
