83519ed41f63cb53084b2942b8d42a02591b08c5
changbin-2022 <poulothy@gmail.com>
2025-05-24 23:58:03 +0300

    Stop tracking dbSequelize.js to respect .gitignore

--- dbSequelize.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('ExchangeRate', 'sa', '12345', { // 1: 'name', 2: 'user', 3: 'password'
-    dialect: 'mssql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
